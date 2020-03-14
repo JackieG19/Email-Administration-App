990828ec10a093bc1db91a5f3b47b36fd0d493d0
Jackie <JGtheophila19@gmail.com>
2020-03-14 15:43:40 -0400

    crating constructor to receive first and last name

--- Email Administration App/src/emailapp/Email.java
+++ Email Administration App/src/emailapp/Email.java
@@ -1,5 +1,15 @@
 package emailapp;
 
 public class Email {
-
+  private String firstName;
+	private String lastName;
+	private String password;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email created: " + this.firstName + " " + this.lastName);
+	}
 }
